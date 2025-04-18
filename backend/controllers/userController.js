6bcf1561e30f6c5624db54d987f98993ee6166d9
Kisekios <dekostump@gmail.com>
2025-04-18 17:17:53 -0500

    Inicializando proyectos web

---
+++ backend/controllers/userController.js
@@ -0,0 +1,22 @@
+const User = require('../models/user');
+
+// Crear un usuario
+exports.createUser = async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// Obtener todos los usuarios
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
